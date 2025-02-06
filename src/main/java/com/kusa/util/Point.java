7ba935f84cccefb8d97964555f3ba318d2257fa0
Kevin Barrios <barrik3@unlv.nevada.edu>
2025-02-06 13:35:56 -0800

    added hashcode and equals overrides + width, height, and center fields

--- src/main/java/com/kusa/util/Point.java
+++ src/main/java/com/kusa/util/Point.java
@@ -1,17 +1,29 @@
 package com.kusa.util;
 
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
 
 public class Point {
 
   private float x;
   private float y;
+  private float w;
+  private float h;
   private Rectangle rect;
+  private Vector2 center;
 
   public Point(float x, float y) {
+    this(x, y, 1f, 1f);
+  }
+
+  //x point, y point, width, height.
+  public Point(float x, float y, float w, float h) {
     this.x = x;
     this.y = y;
-    this.rect = new Rectangle(x, y, 1f, 1f);
+    this.w = w;
+    this.h = h;
+    this.rect = new Rectangle(x, y, w, h);
+    this.center = new Vector2(x + (w * 0.5f), y + (h * 0.5f));
   }
 
   public void setX(float x) {
@@ -32,6 +44,26 @@ public class Point {
 
   public Rectangle getRect() {
     return this.rect;
+  }
+
+  public Vector2 getCenter() {
+    return this.center;
+  }
+
+  @Override
+  public int hashCode() {
+    int hash = 7;
+    hash = 71 * hash + Math.round(this.x);
+    hash = 71 * hash + Math.round(this.y);
+    return hash;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (o == this) return true;
+    if (!(o instanceof Point)) return false;
+    Point o_ = (Point) o;
+    return ((o_.getX() == this.x) && (o_.getY() == this.y));
   }
 
   @Override
